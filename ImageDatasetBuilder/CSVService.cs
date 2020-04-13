7c039674c105e4155fece09515d386603d5dea5e
Jakub Plona <plonajakub@gmail.com>
2020-04-13 20:43:44 +0200

    Add a layer of abstraction for image example representation

--- ImageDatasetBuilder/CSVService.cs
+++ ImageDatasetBuilder/CSVService.cs
@@ -1,15 +1,19 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
 using CsvHelper;
 using CsvHelper.Configuration;
 
+using Point = System.Drawing.Point;
+using ObjectClass = ImageDatasetBuilder.HelperStructures.ObjectClass;
+
 namespace ImageDatasetBuilder
 {
     public class CsvService
     {
-        public class ImageExample
+        public class ImageExampleEntry
         {
             public string Filename { get; set; }
             public string ImageFormat { get; set; }
@@ -17,19 +21,14 @@ namespace ImageDatasetBuilder
             public double Width { get; set; }
             public string Class { get; set; }
             public int XMin { get; set; }
-            public int XMax { get; set; }
             public int YMin { get; set; }
+            public int XMax { get; set; }
             public int YMax { get; set; }
-            
-            public override string ToString()
-            {
-                return "(" + XMin + ", " + YMin + ", " + XMax + ", " + YMax + ", " + Class + ")";
-            }
         }
 
-        public class ImageExampleMap : ClassMap<ImageExample>
+        public class ImageExampleEntryMap : ClassMap<ImageExampleEntry>
         {
-            public ImageExampleMap()
+            public ImageExampleEntryMap()
             {
                 Map(m => m.Filename).Index(0).Name("filename");
                 Map(m => m.ImageFormat).Index(1).Name("img_format");
@@ -37,37 +36,89 @@ namespace ImageDatasetBuilder
                 Map(m => m.Width).Index(3).Name("width");
                 Map(m => m.Class).Index(4).Name("class");
                 Map(m => m.XMin).Index(5).Name("x_min");
-                Map(m => m.XMax).Index(6).Name("x_max");
-                Map(m => m.YMin).Index(7).Name("y_min");
+                Map(m => m.YMin).Index(6).Name("y_min");
+                Map(m => m.XMax).Index(7).Name("x_max");
                 Map(m => m.YMax).Index(8).Name("y_max");
             }
         }
 
-        public List<ImageExample> ReadCsv(string filePath)
+        private static IEnumerable<ImageExampleEntry> ReadCsvEntries(string filePath)
         {
             using (var reader = new StreamReader(filePath))
             using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
-            {    
-                csv.Configuration.RegisterClassMap<ImageExampleMap>();
-                return csv.GetRecords<ImageExample>().ToList();
+            {
+                csv.Configuration.RegisterClassMap<ImageExampleEntryMap>();
+                return csv.GetRecords<ImageExampleEntry>();
             }
         }
 
-        public void WriteCsv(string filePath, List<ImageExample> imageExamples)
+        private static void WriteCsvEntries(string filePath, IEnumerable<ImageExampleEntry> imageExampleEntries)
         {
             using (var writer = new StreamWriter(filePath))
             using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
             {
-                csv.Configuration.RegisterClassMap<ImageExampleMap>();
-                csv.WriteRecords(imageExamples);
+                csv.Configuration.RegisterClassMap<ImageExampleEntryMap>();
+                csv.WriteRecords(imageExampleEntries);
             }
         }
-
-        public void AppendToCsv(string filePath, List<ImageExample> imageExamplesToAppend)
+        
+        public static IEnumerable<ImageExample> ReadCsv(string filePath)
         {
-            var imageExamples = this.ReadCsv(filePath);
+            var csvEntries = ReadCsvEntries(filePath);
+            var imageExamples = new List<ImageExample>();
+            foreach (var csvEntry in csvEntries)
+            {
+                var imageExample = imageExamples.FirstOrDefault(example => example.Filename == csvEntry.Filename);
+                if (imageExample == default(ImageExample))
+                {
+                    imageExample = new ImageExample
+                    {
+                        Filename = csvEntry.Filename,
+                        ImageFormat = csvEntry.ImageFormat,
+                        Height = csvEntry.Height,
+                        Width = csvEntry.Width
+                    };
+
+                    imageExamples.Add(imageExample);
+                }
+                imageExample.Bounds.Add(new Tuple<ObjectClass, Point, Point>(
+                    HelperStructures.ObjectClassMapping.FirstOrDefault(m => m.Value == csvEntry.Class).Key,
+                    new Point(csvEntry.XMin, csvEntry.YMin),
+                    new Point(csvEntry.XMax, csvEntry.YMax)));
+            }
+
+            return imageExamples;
+        }
+
+        public static void WriteCsv(string filePath, IEnumerable<ImageExample> imageExamples)
+        {
+            var imageExampleEntries = new List<ImageExampleEntry>();
+            foreach (var imageExample in imageExamples)
+            {
+                foreach (var (boundClass, minPoint, maxPoint) in imageExample.Bounds)
+                {
+                    imageExampleEntries.Add(new ImageExampleEntry
+                    {
+                        Filename = imageExample.Filename,
+                        ImageFormat = imageExample.ImageFormat,
+                        Height = imageExample.Height,
+                        Width = imageExample.Width,
+                        Class = HelperStructures.ObjectClassMapping[boundClass],
+                        XMin = minPoint.X,
+                        YMin = minPoint.Y,
+                        XMax = maxPoint.X,
+                        YMax = maxPoint.Y
+                    });
+                }
+            }
+            WriteCsvEntries(filePath, imageExampleEntries);
+        }
+
+        public static void AppendToCsv(string filePath, IEnumerable<ImageExample> imageExamplesToAppend)
+        {
+            var imageExamples = ReadCsv(filePath).ToList();
             imageExamples.AddRange(imageExamplesToAppend);
-            this.WriteCsv(filePath, imageExamples);
+            WriteCsv(filePath, imageExamples);
         }
     }
 }
---
+++ ImageDatasetBuilder/ImageExample.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Point = System.Drawing.Point;
+using ObjectClass = ImageDatasetBuilder.HelperStructures.ObjectClass;
+
+namespace ImageDatasetBuilder
+{
+    public class ImageExample
+    {
+        public string Filename { get; set; }
+        public string ImageFormat { get; set; }
+        public double Height { get; set; }
+        public double Width { get; set; }
+
+        /// <summary>
+        /// Single entry: (class, (x_min, y_min), (x_max, y_max))
+        /// </summary>
+        public List<Tuple<ObjectClass, Point, Point>> Bounds { get; } = new List<Tuple<ObjectClass, Point, Point>>();
+
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+            foreach (var (boundClass, minPoint, maxPoint) in Bounds)
+            {
+                builder.Append("(" + boundClass + ", " + 
+                               "(" + minPoint.X + ", " + minPoint.Y + "), " +
+                               "(" + maxPoint.X + ", " + maxPoint.Y + "))" + "\r\n");
+            }
+            return builder.ToString();
+        }
+    }
+}
